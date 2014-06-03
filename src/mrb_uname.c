4b38b68ca8336c7d2936102e436cdc42b94e047f
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-06-03 01:01:50 -0700

    Change Uname#sysname to Uname.sysname

--- src/mrb_uname.c
+++ src/mrb_uname.c
@@ -7,6 +7,8 @@
 */
 
 #include "mruby.h"
+#include "mruby/variable.h"
+#include "mruby/class.h"
 #include "mruby/data.h"
 #include "mrb_uname.h"
 #include <sys/utsname.h>
@@ -21,42 +23,46 @@ static const struct mrb_data_type mrb_uname_data_type = {
   "mrb_uname_data", mrb_free,
 };
 
-static mrb_value mrb_uname_init(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_uname_wrap(mrb_state *mrb, struct RClass *uname,
+    mrb_uname_data *data)
 {
-  mrb_uname_data *data;
+    return mrb_obj_value(Data_Wrap_Struct(mrb, uname, &mrb_uname_data_type,
+          data));
+}
+
+static mrb_uname_data *mrb_uname_get_uname_data(mrb_state *mrb, mrb_value self)
+{
   struct utsname uname_b;
+  mrb_uname_data *data = NULL;
+  mrb_sym uname_sym = mrb_intern_lit(mrb, "@@uname");
 
-  data = (mrb_uname_data *)DATA_PTR(self);
-  if (data) {
-    mrb_free(mrb, data);
+  if (mrb_cv_defined(mrb, self, uname_sym)) {
+    Data_Get_Struct(mrb, mrb_cv_get(mrb, self, uname_sym),
+          &mrb_uname_data_type, data);
+  } else {
+    data = (mrb_uname_data *)mrb_malloc(mrb, sizeof(mrb_uname_data));
+    if (uname(&uname_b) != 0) {
+      mrb_raise(mrb, E_RUNTIME_ERROR, "uname failed");
+    }
+    data->uname_b = uname_b;
+    mrb_cv_set(mrb, self, uname_sym,
+        mrb_uname_wrap(mrb, mrb_class_ptr(self), data));
   }
-  DATA_TYPE(self) = &mrb_uname_data_type;
-  DATA_PTR(self) = NULL;
-
-  data = (mrb_uname_data *)mrb_malloc(mrb, sizeof(mrb_uname_data));
-  if (uname(&uname_b) != 0) {
-    mrb_raise(mrb, E_RUNTIME_ERROR, "uname failed");
-  }
-
-  data->uname_b = uname_b;
-  DATA_PTR(self) = data;
-
-  return self;
+  return data;
 }
 
 static mrb_value mrb_uname_sysname(mrb_state *mrb, mrb_value self)
 {
-  mrb_uname_data *data = DATA_PTR(self);
-
+  mrb_uname_data *data = mrb_uname_get_uname_data(mrb, self);
   return mrb_str_new_cstr(mrb, data->uname_b.sysname);
 }
 
 void mrb_mruby_uname_gem_init(mrb_state *mrb)
 {
     struct RClass *uname;
+
     uname = mrb_define_class(mrb, "Uname", mrb->object_class);
-    mrb_define_method(mrb, uname, "initialize", mrb_uname_init, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, uname, "sysname", mrb_uname_sysname, MRB_ARGS_NONE());
+    mrb_define_class_method(mrb, uname, "sysname", mrb_uname_sysname, MRB_ARGS_NONE());
     DONE;
 }
 
